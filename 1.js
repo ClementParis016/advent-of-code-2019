016961ac3bd54033ea7aa03cb9406a3fbef38007
Clément Paris <clementparis016@gmail.com>
2019-12-07 20:23:33 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,7 +2,7 @@ var modules = $0.textContent.split('\n').slice(0, -1).map(m => parseInt(m, 10))
 
 var getFuel = (m) => {
   var f = Math.floor(m/3) - 2
-	return f > 0 ? f : 0
+  return f > 0 ? f : 0
 }
 
 // Question 1
@@ -16,5 +16,5 @@ var result = modules.reduce((t, m) => {
     t += getFuel(m)
     m = getFuel(m)
   }
-	return t
+  return t
 }, 0)
